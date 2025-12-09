d9213a0cdff12e29c47388e9cf2288b1830d3398
coatpop-collab <popcoat123coatpop@gmail.com>
2025-12-09 12:15:59 +0700

    Lab3+4

---
+++ Lab4/web/page/blog.php
@@ -0,0 +1,35 @@
+<div>My PHP</div>
+<?php
+echo "Hello PHP !!!";
+
+$name = "Coat";
+$age = 22;
+
+var_dump($name);
+?>
+<?= $name." : ".$age  ?> 
+
+<div>
+    <form id="fm_blog">
+        <input type="text" name="blog">
+        <button type="submit">SAVE</button>
+    </form>
+
+</div>
+
+<div>
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+            </tr>
+        </thead>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+            </tr>
+        </thead>
+    </table>
+</div>
